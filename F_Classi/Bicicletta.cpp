0a6a7515d7578356d8f5fe5cdae88b5df39a6f48
Noorlyn <153986179+Noorlyn@users.noreply.github.com>
2024-03-02 09:17:19 +0000

    tris

--- F_Classi/Bicicletta.cpp
+++ F_Classi/Bicicletta.cpp
@@ -9,9 +9,14 @@ class Bicicletta {
   public:
     int raggio;
     string colore;
-    Bicicletta( int r, string c ) {
-        raggio=r;
-        colore=c;
+
+    Bicicletta( ) {
+        raggio=10;
+        colore="Bianco";
+    }
+    Bicicletta (int r, string c){
+        raggio = r;
+        colore = c;
     }
     void presentati() {
         cout << "Buongiorno, sono una bici di colore " << colore << " e con le ruote " << raggio << "!" << endl;
@@ -20,11 +25,11 @@ class Bicicletta {
 
 int main()
 {
-    Bicicletta x(16,"verdino");
-    Bicicletta y(14,"rossastro");
-
+    Bicicletta x;
     x.presentati();
+    Bicicletta y(14,"rossastro");
     y.presentati();
+
 
     return 0;
 }
--- tris/tris.cpp
+++ tris/tris.cpp
@@ -24,18 +24,15 @@ public:
     }
     void stampa_griglia()
     {
-        cout << endl<< "|";
-        cout <<"---|---|---|---|---"<< "|";
-        cout << endl;
+
         for (int i = 0; i < 3; i++)
         {
             for (int j = 0; j < 3; j++)
             {
-                cout << "| " <<griglia[i][j]<< " |" << "\t";
+                cout <<griglia[i][j]<< "   ";
             }
-            cout << endl<< "|";
-            cout <<"---|---|---|---|---"<< "|";
-            cout << endl;
+            cout<<endl;
+
         }
     }
 
