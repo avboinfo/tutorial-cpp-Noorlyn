aca77f02fbe4336104a5a267758752484752a425
Noorlyn <153986179+Noorlyn@users.noreply.github.com>
2024-03-23 10:32:27 +0000

    mastermind

--- X_MasterMind/mastermind.cpp
+++ X_MasterMind/mastermind.cpp
@@ -11,6 +11,7 @@ private:
     static const int DIM_MOSSA_VALIDA = 4;
     int mossa_valida[DIM_MOSSA_VALIDA];
     int codice_segreto[DIM_MOSSA_VALIDA];
+    int posizione_numeri[DIM_MOSSA_VALIDA];
 
     bool sanifica_input(){
         if( DIM_MOSSA_VALIDA != ultima_mossa.size()){
@@ -39,7 +40,26 @@ private:
         for (int i = 0; i < DIM_MOSSA_VALIDA; i++)
         {
             codice_segreto[i] = rand() % 10;
+            std::cout<< codice_segreto[i];
         }
+        std::cout << std::endl;
+    }
+    void assegna_posizioni(){
+
+        for(int i = 0; i < DIM_MOSSA_VALIDA; i++)
+        {
+            posizione_numeri[i] = codice_segreto[i];
+        }
+    }
+    int* copia_vettore(int vettore_originale[DIM_GIOCATA_VALIDA]){
+
+        static int vettore_copia[DIM_GIOCATA_VALIDA];
+
+        for(int i = 0; i < dim vec; i++)
+        {
+            vettore_copia[i]=vettore_originale[i]
+        }
+        return vettore_copia;
     }
 public:
 
@@ -59,14 +79,67 @@ public:
         numero_mosse++;
     }
 
-    void risultato_mossa(){
-        for (int i = 0; i < DIM_MOSSA_VALIDA; i++)
-        {
-            if (codice_segreto[i] == mossa_valida[i] && )
-            {
 
-            }else if ()
-        }
+    void risultato_mossa()
+    {
+        int strike, ball;
+        strike = ball = 0;
+        //Creo una copia del vettore codice per non andare a sovrascrivere il valore originale
+        int *codice_segreto_bis = copia_vettore(codice_segreto, DIM_GIOCATA_VALIDA);
+        //cerco quanti strike e quanti ball
+        for(int i=0;i<DIM_GIOCATA_VALIDA;i++) //Uso i per iterare su mossa_valida
+        {
+            for(int j=0;j<DIM_GIOCATA_VALIDA;j++)//Uso j per iterare su codice segreto_bis
+            {
+                if(mossa_valida[i]==codice_segreto_bis[j]){
+                    if(i==j){
+                        strike++;
+
+                    }else
+                        ball++;
+                    codice_segreto_bis[j]=2;
+                }
+            }
+        }    
+        std::cout<<"Il numero di strike e' : "<<strike<<std::endl;
+        std::cout<<"Il numero di ball e' : "<<ball<<std::endl;
+
     }
+    void risultato_mossa_bis(){
+        int strike, ball;
+        strike = ball = 0;
+
+        int *codice_segreto_bis= copia_vettore(codice_segreto_bis, DIM_MOSSA_VALIDA);
+        copia_vettore_bis(codice_segreto, codice_segreto_bis, DIM_GIOCATA_VALIDA);
+
+        for (int i = 0; i < DIM_GIOCATA_VALIDA; i++)
+        {
+            for (int j = 0; i < DIM_GIOCATA_VALIDA; j++)
+            {
+                if(mossa_valida[i] == codice_segreto_bis[j])
+                {
+                    if(i == j)
+                        strike++;
+                    else
+                        ball++;
+                    codice_segreto_bis[j] = -2;
+                }
+            }
+        }
+        std::cout<< "Il numero di strike e': " << strike << std::endl;
+        std::cout << "Il numero di ball e': " << ball << std::endl;
+    }    
 
 };
+
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    mastermind prova = mastermind();
+
+    cout << "Benvenuto al gioco mastermind!" << endl;
+
+    
+}
