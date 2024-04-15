e39c445bed4083aa9f1a76efa765facc5d54e55e
Noorlyn <153986179+Noorlyn@users.noreply.github.com>
2024-04-15 07:49:57 +0000

    Matrice

--- X_BattleShip/Matrice.cpp
+++ X_BattleShip/Matrice.cpp
@@ -6,31 +6,53 @@
 #include <iostream>
 using namespace std;
 
+const int DIM = 6;
+
 class Matrice
 {
     private:
-        int m[10][10];
+        char m[DIM][DIM];
 
     public:
-        Matrice( int n )
-        {
-            for (int i = 0; i < 10; i++)
+
+        Matrice() {
+
+            for (int i = 0; i < DIM; i++)
             {
-                for(int j = 0; j < 10; j++)
+                for(int j = 0; j < DIM; j++)
                 {
-                    m[i][j] = n;
+                    m[i][j] = 97 + rand()%26;
+                }
+            }
+
+        }
+        Matrice( char c )
+        {
+            for (int i = 0; i < DIM; i++)
+            {
+                for(int j = 0; j < DIM; j++)
+                {
+                    m[i][j] = c;
                 }
             }
         }
 
+        char get( int x, int y)
+        {
+            return m[x][y];
+        }
+        void put( int x, int y, char c)
+        {
+            m[x][y] = c;
+        }
         void StampaMatrice (  )
         {
             cout << "-------------------------------------------- " << endl << endl;
-            for (int i = 0; i < 10; i++)
+            for (int i = 0; i < DIM; i++)
             {
-                for(int j = 0; j < 10; j++)
+                for(int j = 0; j < DIM; j++)
                 {
-                    cout << m[i][j] << "\t";
+                    printf(" %c ", m[i][j]);
                 }
                 cout << endl;
             }
@@ -38,14 +60,66 @@ class Matrice
 
         };
 
+        void bomb()
+        {
+            int x = rand() % DIM;
+            int y = rand() % DIM;
+            m[x][y] = '*';
+        }
+
+        void placeHorizontalShip(int len)
+        {
+            if (len <= 0 || len >= DIM )
+            {
+                return;
+            }
+            int x = rand() % DIM;
+            int y = rand() % (DIM - len);
+            for (int i = 0; i < len; i++)
+            {
+                m[x][y+i] = 'O'; 
+            }
+        }
+        
+        void placeVerticalShip(int len)
+        {
+            if (len <= 0 || len >= DIM )
+            {
+                return;
+            }
+            int x = rand() % (DIM - len);
+            int y = rand() % DIM;
+            for (int i = 0; i < len; i++)
+            {
+                m[x+1][y] = 'O'; 
+            }
+        }
 
 
 
 };
 int main()
 {
+    srand(time(NULL));
+    Matrice mappa = Matrice( '-' );
+    Matrice campo = Matrice( '.');
+    mappa.StampaMatrice();
+    campo.placeHorizontalShip (3);
+    campo.placeVerticalShip (4);
+    campo.placeHorizontalShip (1);
+    campo.placeVerticalShip (5);
+    for (int i = 0; i < 20; i++)
+    {
+        int x = rand()% DIM;
+        int y = rand()% DIM;
+        if (campo.get( x, y) == 'O')
+        {
+            mappa.put(x,y,'*');
+        }
+    }
 
-    Matrice m1 = Matrice( 80 );
-    m1.StampaMatrice();
+    mappa.StampaMatrice();
+
+
 
 }
